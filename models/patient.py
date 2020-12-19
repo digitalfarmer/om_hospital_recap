5d75ce621e145ff5e607b2d76abb69748003a93b
digitalfarmer <adesbsp@gmail.com>
2020-12-19 12:04:16 +0700

    adding record name and field name

--- models/patient.py
+++ models/patient.py
@@ -3,9 +3,10 @@ from odoo import fields, models, api, _
 class HospitalPatient(models.Model):
     _name ='hospital.patient'
     _description= 'Manage Hospital'
-
+    _rec_name= 'patient_name'
 
     patient_name = fields.Char('Patient Name', required=True)
     patient_age = fields.Integer('Age')
     notes = fields.Text('Note')
     image = fields.Binary('Image')
+    name= fields.Char('Test')
