2d20994d3f7ba5790254ade0bbafa27265d3d0a9
digitalfarmer <adesbsp@gmail.com>
2020-12-26 10:33:54 +0700

    add sequence data for appointment

--- models/appointment.py
+++ models/appointment.py
@@ -8,7 +8,7 @@ class HospitalAppointment(models.Model):
     @api.model
     def create(self, vals):
         if vals.get('name', _('New')) == _('New'):
-            vals['name'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or _('New')
+            vals['name'] = self.env['ir.sequence'].next_by_code('hospital.appointment.sequence') or _('New')
         result = super(HospitalAppointment, self).create(vals)
         return result
     
